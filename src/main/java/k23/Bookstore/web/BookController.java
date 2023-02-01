179b11376ba789b48504e1652134bbfad5ee5444
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2023-02-01 19:47:06 +0200

    Tehtävä 3.2

--- src/main/java/k23/Bookstore/web/BookController.java
+++ src/main/java/k23/Bookstore/web/BookController.java
@@ -2,11 +2,20 @@ package k23.Bookstore.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import k23.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	@Autowired
 	BookRepository bookRepository;
+	
+	@GetMapping("booklist")
+	public String showBooks(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist";
+	}
 
 }
