4e8c64193cfce3b07cf61af2443625de5ee0835d
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2023-02-04 15:26:20 +0200

    Tehtävä 3.5

--- src/main/java/k23/Bookstore/BookstoreApplication.java
+++ src/main/java/k23/Bookstore/BookstoreApplication.java
@@ -24,9 +24,9 @@ public CommandLineRunner demo(BookRepository repository) {return (args) -> {
 	//double price
 	// int publicationYear
 	
-	repository.save(new Book("Otsikko1", "tekijä1", 2001, "isbn1", 11));
-	repository.save(new Book("Otsikko2", "tekijä2", 2002, "isbn2", 22));
-	repository.save(new Book("Otsikko3", "tekijä3", 2003, "isbn3", 33));
+	repository.save(new Book("Kirja1", "Etunimi Sukunimi1", 2001, "12345-6", 11));
+	repository.save(new Book("Kirja2", "Etunimi Sukunimi2", 2002, "23456-7", 22));
+	repository.save(new Book("Kirja3", "Etunimi Sukunimi3", 2003, "34567-8", 33));
 };
 }
 }
